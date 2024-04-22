61914dadca5eec9bda1a80c6dde24a09373a85b2
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 19:03:10 -0500

    primer GUIA12

---
+++ src/main/java/com/mycompany/guias12/GUIAS12.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias12;
+
+import igui.pantallas12;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS12 {
+
+    public static void main(String[] args) {
+        new pantallas12().setVisible(true);
+    }
+}
